b28fc6be881358856829e78fdf28f15282402675
wswddl <4tri4tri@gmail.com>
2025-01-23 09:28:10 +0800

    add MiloIce.java

--- src/main/java/MiloIce.java
+++ src/main/java/MiloIce.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class MiloIce {
+
+    public static void printStraightLine() {
+        int lineLength = 50;
+        for (int i = 0; i < lineLength; i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
         String logo = " __    __   __   __        ____       _______    _____   ______        \n"
@@ -8,22 +19,26 @@ public class MiloIce {
              + "|   ||   | |  | |  |___  | \\__/ |     __| |__  |   \\__  | |____        \n"
              + "|___||___| |__| |______|  \\____/     |_______|  \\_____| |______|       ";
         System.out.println(logo);
-        for (int i = 0; i < 30; i++) {
-            System.out.print("_");
-        }
+        printStraightLine();
+        System.out.println("Hello! I'm Milo Ice\n" + "What can I do for you?");
+        printStraightLine();
 
-        System.out.print("\n" + """
-                Hello! I'm Milo Ice
-                What can I do for you?m
-                """);
-        for (int i = 0; i < 30; i++) {
-            System.out.print("_");
+        // Scanning for input
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                printStraightLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printStraightLine();
+                break;
+            } else {
+                printStraightLine();
+                System.out.println(input); // echo
+                printStraightLine();
+            }
         }
-        System.out.print("\n" + """
-                Bye. Hope to see you again soon!
-                """);
-        for (int i = 0; i < 30; i++) {
-            System.out.print("_");
-        }
+        scanner.close();
     }
 }
